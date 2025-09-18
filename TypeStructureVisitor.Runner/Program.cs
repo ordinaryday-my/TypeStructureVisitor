43e2249c8db0d113316d46b29917fe3e1c952420
天健 王 <OrdinaryDay21@outlook.com>
2025-09-19 00:59:07 +0800

    不小心忘提交了

---
+++ TypeStructureVisitor.Runner/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+using TypeStructureVisitor;
+
+var visitor = new TypeStructureVisitor.TypeStructureVisitor(typeof(System.Timers.Timer), IndentationOption.Default);
+var writer = new StreamWriter("a.log", Encoding.UTF8, new FileStreamOptions()
+{
+    Mode = FileMode.Create,
+    Access = FileAccess.Write,
+    Share = FileShare.Read
+});
+visitor.Visit(writer);
